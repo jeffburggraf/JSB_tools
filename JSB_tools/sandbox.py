60301bfb031843ab3975aadc20d620679aa1ede3
BuildTools <unconfigured@null.spigotmc.org>
2021-06-10 17:11:37 -0600

    N

--- JSB_tools/sandbox.py
+++ JSB_tools/sandbox.py
@@ -1,18 +1,64 @@
-from JSB_tools.nuke_data_tools import Nuclide
+# from JSB_tools.nuke_data_tools import Nuclide
 from matplotlib import pyplot as plt
-s = Nuclide.from_symbol('U238')
-from uncertainties import unumpy as unp
-from openmc.data import FissionProductYields
+import matplotlib
+from mplhep import histplot
+import  numpy as np
+# print(matplotlib.__version__)
+# # s = Nuclide.from_symbol('U238')
+# from uncertainties import unumpy as unp
+# # from openmc.data import FissionProductYields
+# bins = np.arange(0, 100)
+# y, _ = np.histogram(20*np.random.rand(1000) + 50, bins=bins)
+# yerr = np.sqrt(y)
+# print(y)
+#
+# histplot(y, bins=bins, yerr = yerr, ds='steps-mid')
+# # n = Nuclide.from_symbol('U238')
+import matplotlib.pyplot as plt
+import numpy as np
+import inspect
 
-y = FissionProductYields("/Users/jeffreyburggraf/Downloads/UKFY41data/ukfy4_1p/U238")
-print(y.nuclide)
+def hist_errorbars( data, xerrs=True, *args, **kwargs) :
+    """Plot a histogram with error bars. Accepts any kwarg accepted by either numpy.histogram or pyplot.errorbar"""
+    # pop off normed kwarg, since we want to handle it specially
+    norm = False
+    if 'normed' in kwargs.keys() :
+        norm = kwargs.pop('normed')
 
-# ergs_n, yield_n = s.independent_neutron_fission_yield('Xe139')
-# ergs_p, yield_p = s.cumulative_neutron_fission_yield('Xe139')
-# plt.errorbar(ergs_n, unp.nominal_values(yield_n), unp.std_devs(yield_n),  label='independent yield')
-# plt.errorbar(ergs_p, unp.nominal_values(yield_p), unp.std_devs(yield_p), label='cumulative yield')
-# plt.title('X-139 yield from U-238(p,F)')
-# plt.xlabel('Incident proton energy [MeV]')
-# plt.ylabel('yield')
-# plt.legend()
-# plt.show()
+    # retrieve the kwargs for numpy.histogram
+    histkwargs = {}
+    for key, value in kwargs.iteritems() :
+        if key in inspect.getargspec(np.histogram).args :
+            histkwargs[key] = value
+
+    histvals, binedges = np.histogram( data, **histkwargs )
+    yerrs = np.sqrt(histvals)
+
+    if norm :
+        nevents = float(sum(histvals))
+        binwidth = (binedges[1]-binedges[0])
+        histvals = histvals/nevents/binwidth
+        yerrs = yerrs/nevents/binwidth
+
+    bincenters = (binedges[1:]+binedges[:-1])/2
+
+    if xerrs :
+        xerrs = (binedges[1]-binedges[0])/2
+    else :
+        xerrs = None
+
+    # retrieve the kwargs for errorbar
+    ebkwargs = {}
+    for key, value in kwargs.iteritems() :
+        if key in inspect.signature(plt.errorbar).k :
+            histkwargs[key] = value
+    out = plt.errorbar(bincenters, histvals, yerrs, xerrs, fmt=".", **ebkwargs)
+
+    if 'log' in kwargs.keys() :
+        if kwargs['log'] :
+            plt.yscale('log')
+
+    if 'range' in kwargs.keys() :
+        plt.xlim(*kwargs['range'])
+
+    return out
